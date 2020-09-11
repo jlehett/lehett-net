0e0860fdf5660c6061f8c92942f1ca0e80bf359d
jlehett <jlehett117@knights.ucf.edu>
2020-09-10 21:53:27 -0400

    Testing server.

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const server = http.createServer(app);
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 4000;
+const http = require('http');
+const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("This is from express.js");
+});
 
 app.listen(PORT, function() {
     console.log('Server is running on Port: ' + PORT);
