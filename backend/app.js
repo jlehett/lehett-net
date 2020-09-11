41b1c4e9f3fb616d8aff6a512beba3e0e562f6bd
jlehett <jlehett117@knights.ucf.edu>
2020-09-10 21:55:55 -0400

    Switch port to 80

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 3000;
+const PORT = 80;
 
 app.use(cors());
 app.use(bodyParser.json());
